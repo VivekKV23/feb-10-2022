adb20651060420f87e44db02f1e1c052924ea2fd
Vivek <mailvivekkv@gmail.com>
2022-02-07 20:03:28 +0530

    comitted

---
+++ login.js
@@ -0,0 +1,13 @@
+function validate(){
+
+var username= document.getElementById("username").value;
+var password = document.getElementById("password").value;
+
+
+if(username=="Vnkithv" && password=="happy21"){
+  window.open("front/index.html");
+}else{
+  alert("Fuck off!");
+}
+
+}

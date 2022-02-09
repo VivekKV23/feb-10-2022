3d78c44632bb279c88ac3b554d12e22e4b93f260
VivekKV23 <74184283+VivekKV23@users.noreply.github.com>
2022-02-09 18:16:56 +0530

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ var password = document.getElementById("password").value;
 
 
 if(username=="Vnkithv" && password=="happy21"){
-  window.open("front/index.html");
+  window.open("front/index.html","_self");
 }else{
   alert("Fuck off!");
 }
